45fc761b923e1abd20da6be790cba2b34ba6241d
Manyush Aroshis <manyusharoshis153@gmail.com>
2026-08-10 21:34:32 +0530

    Add data types and operators examples

---
+++ 01_Basics/data_types.py
@@ -0,0 +1,13 @@
+# data_types.py
+
+name = "Manyush"
+age = 20
+cgpa = 8.5
+is_student = True
+
+print("Value\t\tType")
+print("------------------------")
+print(name, "\t", type(name))
+print(age, "\t\t", type(age))
+print(cgpa, "\t\t", type(cgpa))
+print(is_student, "\t", type(is_student))

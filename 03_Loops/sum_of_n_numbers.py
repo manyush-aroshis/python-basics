2cc00900f3afaa3be23794c41f0632eaf271f2fa
Manyush Aroshis <manyusharoshis153@gmail.com>
2026-08-11 23:27:35 +0530

    Add loop examples

---
+++ 03_Loops/sum_of_n_numbers.py
@@ -0,0 +1,10 @@
+# sum_of_n_numbers.py
+
+n = int(input("Enter a number: "))
+
+total = 0
+
+for i in range(1, n + 1):
+    total += i
+
+print("Sum =", total)
---
+++ 03_Loops/while_loop.py
@@ -0,0 +1,7 @@
+# while_loop.py
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i += 1
